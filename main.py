8c58315c9ea080c36fa4c9e48ce84caa7a69c44d
Matheus1199 <89140928+Matheus1199@users.noreply.github.com>
2022-07-01 18:35:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,40 @@ password = 'sql@964012'
 conexao = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
 cursor = conexao.cursor()
+cursor2 = conexao.cursor()
 
-id_cliente = 295
+id = 1
 
-comando = f"""SELECT top 3 C.Nome, P.Data, P.Valor_Total
+id_cliente = """SELECT c.Id_Cliente
 FROM TbL_Clientes C, Tbl_Pedidos P
-WHERE  p.Cod_Cliente = C.Id_Cliente and p.Cod_Cliente =  {id_cliente}
+WHERE  p.Cod_Cliente = C.Id_Cliente and p.Data >= '2022' and c.Ativo = 1 and Id_Cliente <> 1
 ORDER BY p.Data desc"""
 
-cursor.execute(comando) 
-row = cursor.fetchone() 
+comando = f"""SELECT top 3 C.Nome, P.Data, P.Valor_Total
+    FROM TbL_Clientes C, Tbl_Pedidos P
+    WHERE  p.Cod_Cliente = C.Id_Cliente and p.Cod_Cliente = {id}
+    ORDER BY p.Data desc"""
+
+
+
+cursor.execute(id_cliente) 
+row = cursor.fetchone()
 while row: 
-    print(row[0], row[1], row[2])
+    print(row[0])
+    id = row[0]
+    comando = f"""SELECT top 3 C.Nome, P.Data, P.Valor_Total
+    FROM TbL_Clientes C, Tbl_Pedidos P
+    WHERE  p.Cod_Cliente = C.Id_Cliente and p.Cod_Cliente = {id}
+    ORDER BY p.Data desc"""
+    cursor.execute(comando)  
+    row2 = cursor.fetchone()
+    while row2:
+        print(id)
+        print(row2[0])
+        row2 = cursor.fetchone()
     row = cursor.fetchone()
+
+
+
 
 print("Conexão concluida")
