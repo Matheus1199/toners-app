ee80a87ddbd5396dc4e4d5ec4255b895a3ae6278
Matheus1199 <89140928+Matheus1199@users.noreply.github.com>
2022-07-05 17:50:19 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,37 +1,37 @@
 import pyodbc
-import pandas as pd 
+import pandas as pd
+import numpy as np
 
-def retornar_conexap_sql():
-    server = '192.168.10.250' 
-    database = 'barsottisolucoes' 
-    username = 'sa' 
-    password = 'sql@964012' 
-    string_conexao = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password + ';TrustedConnection=true'
-    conexao = pyodbc.connect(string_conexao)
-    return conexao
+server = '192.168.10.250' 
+database = 'barsottisolucoes' 
+username = 'sa' 
+password = 'sql@964012' 
+conexao = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
-conn = retornar_conexap_sql()
-
-cursor = conn.cursor()
+cursor = conexao.cursor()
 
 id = 1
 
 id_cliente = """SELECT c.Id_Cliente
-FROM TbL_Clientes C, Tbl_Pedidos P
-WHERE  p.Cod_Cliente = C.Id_Cliente and p.Data >= '2022' and c.Ativo = 1 and Id_Cliente <> 1
-ORDER BY p.Data desc"""
+    FROM TbL_Clientes C, Tbl_Pedidos P
+    WHERE  p.Cod_Cliente = C.Id_Cliente and p.Data >= '2022' and c.Ativo = 1 and Id_Cliente <> 1
+    ORDER BY p.Data desc"""
 
 comando = f"""SELECT top 3 C.Nome, P.Data, P.Valor_Total
     FROM TbL_Clientes C, Tbl_Pedidos P
     WHERE  p.Cod_Cliente = C.Id_Cliente and p.Cod_Cliente = {id}
     ORDER BY p.Data desc"""
 
-comando2 = f"""SELECT C.Nome
-    FROM TbL_Clientes C"""
+tabela = pd.read_sql(id_cliente, conexao)
 
-tabela = pd.read_sql(comando2, conn)
+id = tabela.to_numpy()
+print(id[0])
 
-tabela.head()
+tabela2 = pd.read_sql(comando, conexao)
 
-tabela["Nome"].head()
 
+#print(tabela.loc[0:0])
+
+print(tabela2)
+
+#print(tabela["Id_Cliente"].head())
