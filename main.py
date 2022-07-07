2c9b6662d3193ec4da07557208dff2f49dfc28a6
Matheus1199 <89140928+Matheus1199@users.noreply.github.com>
2022-07-07 12:31:33 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pyodbc 
+import pyodbc
+import pandas as pd
+import numpy as np
 
 server = '192.168.10.250' 
 database = 'barsottisolucoes' 
@@ -7,40 +9,30 @@ password = 'sql@964012'
 conexao = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
 cursor = conexao.cursor()
-cursor2 = conexao.cursor()
 
-id = 1
 
 id_cliente = """SELECT c.Id_Cliente
-FROM TbL_Clientes C, Tbl_Pedidos P
-WHERE  p.Cod_Cliente = C.Id_Cliente and p.Data >= '2022' and c.Ativo = 1 and Id_Cliente <> 1
-ORDER BY p.Data desc"""
-
-comando = f"""SELECT top 3 C.Nome, P.Data, P.Valor_Total
     FROM TbL_Clientes C, Tbl_Pedidos P
-    WHERE  p.Cod_Cliente = C.Id_Cliente and p.Cod_Cliente = {id}
+    WHERE  p.Cod_Cliente = C.Id_Cliente and p.Data >= '2022' and c.Ativo = 1 and Id_Cliente <> 1
     ORDER BY p.Data desc"""
 
 
+tabela = pd.read_sql(id_cliente, conexao)
 
-cursor.execute(id_cliente) 
-row = cursor.fetchone()
-while row: 
-    print(row[0])
-    id = row[0]
+
+cont = 0
+id = tabela.to_numpy()
+cliente = int(id[cont])
+
+
+for cliente in range(cont, len(id)):
     comando = f"""SELECT top 3 C.Nome, P.Data, P.Valor_Total
-    FROM TbL_Clientes C, Tbl_Pedidos P
-    WHERE  p.Cod_Cliente = C.Id_Cliente and p.Cod_Cliente = {id}
-    ORDER BY p.Data desc"""
-    cursor.execute(comando)  
-    row2 = cursor.fetchone()
-    while row2:
-        print(id)
-        print(row2[0])
-        row2 = cursor.fetchone()
-    row = cursor.fetchone()
+        FROM TbL_Clientes C, Tbl_Pedidos P
+        WHERE  p.Cod_Cliente = C.Id_Cliente and p.Cod_Cliente = {cliente}
+        ORDER BY p.Data desc"""
+    tabela2 = pd.read_sql(comando, conexao)
+    print(tabela2)
+    cont += 1
 
-
-
-
-print("Conexão concluida")
+var = tabela2.to_numpy()
+print(var[0, 1])
